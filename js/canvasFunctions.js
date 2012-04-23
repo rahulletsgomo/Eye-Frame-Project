b187be1a07ccb96a59af4e09ed46a8a2c8476f42
Rahul Meghlan <rahul@letsgomo.com>
2012-04-23 18:17:11 +0530

    click events and slider functions taken to different js

--- js/canvasFunctions.js
+++ js/canvasFunctions.js
@@ -43,3 +43,68 @@ function clearCanvas() {
 function defaultImage() {
     context.drawImage(sideBar, sideBarVariables.x, sideBarVariables.y, sideBarWidth, sideBarHeight);
 }
+
+function getClickEvents() {
+
+    $("#sideBarMoveDown").click(function () {
+        var angle = 0.5;
+        sideBarMoveDown(angle);
+    })
+    $("#sideBarMoveUp").click(function () {
+        sideBarMoveUp();
+    })
+    $("#sideBarMoveIn").click(function () {
+        var reduceWidth = 230;
+        var reduceHeight = 2;
+        sideBarMoveIn(reduceWidth, reduceHeight);
+    })
+    $("#sideBarMoveOut").click(function () {
+        moveOut();
+    })
+
+    $("#frontBarMoveDown").click(function () {
+        log("front bar move down !")
+//                var angle = 0.5;
+//                moveDown(angle);
+    })
+    $("#frontBarMoveUp").click(function () {
+//                moveUp();
+        log("front bar move up !")
+
+    })
+    $("#frontBarMoveIn").click(function () {
+//                var reduceWidth = 230;
+//                var reduceHeight = 2;
+//                moveIn(reduceWidth, reduceHeight);
+        log("front bar move in !")
+
+    })
+    $("#frontBarMoveOut").click(function () {
+        log("front bar move out !")
+//                moveOut();
+    })
+
+}
+
+function getSliderDraggable() {
+    $("#specs").draggable();
+
+    $("#slider").slider({
+        value:70,
+        min:20,
+        max:70,
+        step:10,
+        slide:function (event, ui) {
+            $("#heightBox").val(ui.value);
+            var getHeight = parseInt($("#heightBox").val());
+            $("#frontBar").attr("height", getHeight);
+        }
+    });
+    $("#heightBox").val($("#slider").slider("value"));
+
+}
+
+
+function log(message) {
+    console.log(message);
+}
