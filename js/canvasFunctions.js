fe0ae7f7f2de44367bbe1a3eba4f8372a213a0ba
Rahul Meghlan <rahul@letsgomo.com>
2012-04-23 17:54:06 +0530

    started working on the rotation of the front bar

---
+++ js/canvasFunctions.js
@@ -0,0 +1,45 @@
+function sideBarMoveDown(angle) {
+    context.save();
+    clearCanvas();
+    context.translate(sideBarVariables.x, sideBarVariables.y);
+    context.rotate(angle);
+    context.drawImage(sideBar, sideBarVariables.origin, sideBarVariables.origin, sideBarWidth, sideBarHeight);
+    isMovedDown = true;
+}
+
+function sideBarMoveUp() {
+    if (isMovedDown) {
+        console.log("Inside the move up function ...");
+        context.restore();
+        clearCanvas();
+        defaultImage();
+        isMovedDown = false;
+    }
+}
+
+function sideBarMoveIn(reduceWidth, reduceHeight) {
+    clearCanvas();
+    context.drawImage(sideBar, sideBarVariables.x, sideBarVariables.y + 2, (sideBarWidth - reduceWidth), (sideBarHeight) - reduceHeight);
+    isMovedIn = true;
+}
+
+function moveOut() {
+    if (isMovedIn) {
+        try {
+            clearCanvas();
+            defaultImage();
+            isMovedIn = false;
+        }
+        catch (exception) {
+            console.log("The following exception occured : " + exception)
+        }
+    }
+}
+
+function clearCanvas() {
+    context.clearRect(sideBarVariables.x, sideBarVariables.y, sideBarHeight + 100, sideBarWidth);
+}
+
+function defaultImage() {
+    context.drawImage(sideBar, sideBarVariables.x, sideBarVariables.y, sideBarWidth, sideBarHeight);
+}
---
+++ js/globalVars.js
@@ -0,0 +1,13 @@
+//GLobal Variables
+sideBarVariables = {"x":8, "y" : 168, "origin" : 0, "width": 10, "height" : 20}
+
+//boolean variables for checking the motion of the object
+isMovedIn = false;
+isMovedDown = false;
+
+//initialise sideBar variables
+sideBarWidth = 0;
+sideBarHeight = 0;
+
+
+
