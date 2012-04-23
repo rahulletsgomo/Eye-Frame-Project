87318bf94fd95e44e3a3258ca5d61e3464b219ee
Rahul Meghlan <rahul@letsgomo.com>
2012-04-23 19:43:18 +0530

    somewhat refactored the code

--- js/canvasFunctions.js
+++ js/canvasFunctions.js
@@ -39,10 +39,38 @@ function clearCanvas() {
     sideBarContext.clearRect(sideBarVariables.x, sideBarVariables.y, sideBarHeight + 100, sideBarWidth);
 }
 
-function defaultImage() {
-    sideBarContext.drawImage(sideBar, sideBarVariables.x, sideBarVariables.y, sideBarWidth, sideBarHeight);
+function defaultImage(getImageType) {
+    if (getImageType == "sideBar") {
+        sideBarContext.drawImage(sideBar, sideBarVariables.x, sideBarVariables.y, sideBarWidth, sideBarHeight);
+    }
+    else {
+        frontBarContext.drawImage(frontBar, frontBarVariables.x, frontBarVariables.y, frontBarWidth, frontBarHeight);
+    }
 }
 
+function initCanvas() {
+    sideBarCanvas = document.getElementById("sideBarCanvas");
+    sideBarContext = sideBarCanvas.getContext("2d");
+    frontBarCanvas = document.getElementById("sideBarCanvas");
+    frontBarContext = sideBarCanvas.getContext("2d");
+}
+
+function initImages() {
+    sideBar = new Image();
+    sideBar.src = "images/4_s.png";
+    sideBar.onload = function () {
+        sideBarWidth = (sideBar.width - sideBarVariables.width);
+        sideBarHeight = (sideBar.height - sideBarVariables.height)
+        defaultImage("sideBar");
+    }
+    frontBar = new Image();
+    frontBar.src = "images/4_c.png";
+    frontBar.onload = function () {
+        frontBarWidth = (frontBar.width - frontBarVariables.width);
+        frontBarHeight = (frontBar.height - frontBarVariables.height)
+        defaultImage("frontBar");
+    }
+}
 
 
 
--- js/globalVars.js
+++ js/globalVars.js
@@ -1,13 +1,24 @@
 //GLobal Variables
+frontBarVariables = {"x":8, "y" : 168, "origin" : 0, "width": 10, "height" : 20}
 sideBarVariables = {"x":8, "y" : 168, "origin" : 0, "width": 10, "height" : 20}
 
 //boolean variables for checking the motion of the object
 isMovedIn = false;
 isMovedDown = false;
 
-//initialise sideBar variables
+//initialise height X width variables
 sideBarWidth = 0;
 sideBarHeight = 0;
+frontBarWidth = 0;
+frontBarHeight = 0;
+
+//initialise canvas and image Objects
+sideBarCanvas = {};
+sideBarContext = {};
+sideBar = {};
+frontBarCanvas = {};
+frontBarContext = {};
+frontBar = {};
 
 
 
